0d62954ee9ba8a86b07929457e0286bb930b4f54
mehdi <13610522>
2023-01-31 16:54:01 +0330

    check add self to method

--- ch1.rb
+++ ch1.rb
@@ -6,11 +6,35 @@ end
 
 class Dog < Pet
   attr_accessor :test
+  def bark
+    puts "barking..."
+  end
 end
 
+class Cat < Pet
+  def meow(opo)
+    puts "#{self.name} says #{opo}"
+  end
+end
 
 d = Dog.new
-
 d.name = "moucha"
 
+
+10.times do
+  puts "hello"
+end
+
+1.upto(20) {|number| puts number }
+5.step(50, 5) { |number|  puts number}
+
+5.step(50, 3) do |number|
+  puts number
+end
+
 puts d.name
+puts d.bark
+
+c = Cat.new
+c.name = "mmmm"
+c.meow("xsxs")
