dc0a5d335f3ec47e37c737f92713b083d5ad12ce
mehdi <13610522>
2023-02-05 15:08:19 +0330

    chapter 3 continued

--- chapter3.rb
+++ chapter3.rb
@@ -48,7 +48,79 @@ puts "this is a test".sub(/^..../, "hello")
 puts "this is a test".sub(/....$/, "bye")
 
 "xyz".scan(/./) { |letter|  puts letter}
-
 "this is a test".scan(/../) { |letter|  puts letter}
-
 "this is a test".scan(/\w\w/) { |letter|  puts letter}
+"This is a test".scan(/[a-m]/) { |x| puts x}
+
+puts "**********************************************array setion ********************************************************"
+x = [1, 2, 3, 4]
+puts x[2]
+
+x[2] += 1
+puts x[2]
+
+y = []
+y << "hello"
+
+y.push("world")
+
+puts y[0]
+puts y
+
+
+x = []
+
+x << "World"
+x << "play"
+x << "found"
+
+puts x.length
+puts x.pop
+puts x.pop
+puts x.length
+
+x = ["world", "play", "fun"]
+puts x.join
+puts x.join(",")
+
+puts "This is a test".scan(/\w/).join(',')
+puts "This is a test".scan(/\w/).inspect
+puts "Words with lots of spaces".split(/\s+/).inspect
+p "Words with lots of spaces".split(/\s+/)
+
+[1, "test", 2, 3, 4,].each { |elemets| puts elemets.to_s + "X" }
+p [1, 2, 3, 4, 5, 6].collect { |elemets| elemets * 2}
+
+a = [1, "test", 2, 3, 4]
+i = 0
+while (i < a.length)
+  puts a[i].to_s + "X"
+  i += 1
+end
+
+x = [1,2,3,4,5]
+y = [1,2,3]
+z = x - y
+p z
+
+x = []
+puts "x is empty" if x.empty?
+
+x = [1, 2, 3]
+
+p x.include?("x")
+p x.include?(2)
+p x.first(2).join(",")
+p x.last(2).join(",")
+
+p x.reverse
+p x.length
+x.length.times do
+  puts "hello"
+end
+
+
+
+x.length.times do |i|
+  puts "hello " + x[i].to_s
+end
